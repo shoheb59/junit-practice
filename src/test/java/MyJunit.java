077e5aeacb3ae5bee1a46b5a02d553d2c1d87e10
shoheb59 <hasnat.shoheb@gmail.com>
2022-04-25 19:23:36 +0600

    Seond class

--- src/test/java/MyJunit.java
+++ src/test/java/MyJunit.java
@@ -9,8 +9,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.interactions.Actions;
 
+import java.sql.Driver;
 import java.time.Duration;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class MyJunit {
@@ -51,10 +54,54 @@ public class MyJunit {
         WebElement image1 = driver.findElement(By.cssSelector("img"));
         Assert.assertTrue(String.valueOf(image1.isDisplayed()),true);
     }
+
+    @Test
+    public void writeSomthing()
+    {
+        driver.get("https://demoqa.com/text-box");
+        //WebElement txtUname = driver.findElement(By.id("userName"));
+        //txtUname.sendKeys("Hasnat Shoheb");
+
+        List <WebElement> elements = driver.findElements(By.tagName("input"));
+
+        elements.get(0).sendKeys("Hasnat");
+        elements.get(1).sendKeys("hasnat@gmail.com");
+
+        driver.findElement(By.id("submit")).click();
+
+
+
+    }
+
+    @Test
+    public void clickButton()
+    {
+        driver.get("https://demoqa.com/buttons");
+        List <WebElement>  element = driver.findElements(By.tagName("button"));
+        element.get(3).click();
+
+        Actions actions = new Actions(driver);
+        actions.doubleClick(element.get(1)).perform();
+
+        //Use contetClick for right click
+        actions.contextClick(element.get(2)).perform();
+
+        String doubleClick = driver.findElement(By.id("doubleClickMessage")).getText();
+        String rightClick = driver.findElement(By.id("rightClickMessage")).getText();
+        String dynamicClick = driver.findElement(By.id("ue626")).getText();
+
+        Assert.assertTrue(doubleClick.contains("You have done a double click"));
+        Assert.assertTrue(rightClick.contains("You have done a right click"));
+        Assert.assertTrue(dynamicClick.contains("You have done a dynamic click"));
+
+
+
+
+    }
     @After
     public void closeDriver()
     {
-        driver.close(); //Specifiq Tab jodi keta dita cy
+        //driver.close(); //Specific Tab jodi keta dita cy
         //driver.quit();
     }
 
